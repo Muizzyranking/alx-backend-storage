d57e25a2b1662d70d3a4f9b90161bccedf88911f
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-14 18:36:31 +0100

    (fix): type annotation
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,7 @@ A module for using the Redis NoSQL database
 
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -17,7 +18,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: bytes) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis database"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
