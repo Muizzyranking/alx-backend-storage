313abbdb158400f928bd149962695d6240462702
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-15 04:44:54 +0100

    (fix): type annotation
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -54,29 +54,24 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
-def replay(fn: Callable) -> None:
-    '''Displays the call history of a Cache class' method.
-    '''
-    if fn is None or not hasattr(fn, '__self__'):
-        return
-    redis_store = getattr(fn.__self__, '_redis', None)
-    if not isinstance(redis_store, redis.Redis):
-        return
-    fxn_name = fn.__qualname__
-    in_key = '{}:inputs'.format(fxn_name)
-    out_key = '{}:outputs'.format(fxn_name)
-    fxn_call_count = 0
-    if redis_store.exists(fxn_name) != 0:
-        fxn_call_count = int(redis_store.get(fxn_name))
-    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
-    fxn_inputs = redis_store.lrange(in_key, 0, -1)
-    fxn_outputs = redis_store.lrange(out_key, 0, -1)
-    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
-        print('{}(*{}) -> {}'.format(
-            fxn_name,
-            fxn_input.decode("utf-8"),
-            fxn_output,
-        ))
+def replay(method: Callable[['Cache'], None]) -> None:
+    """Replay the history of calls to a particular function."""
+    redis_instance = method.__self__._redis
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+
+    # Fetch the inputs and outputs from Redis
+    inputs = redis_instance.lrange(input_key, 0, -1)
+    outputs = redis_instance.lrange(output_key, 0, -1)
+
+    # Display the number of times the method was called
+    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
+
+    # Iterate through the inputs and outputs and display them
+    for input_value, output_value in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(
+            method.__qualname__, input_value.decode('utf-8'),
+            output_value.decode('utf-8')))
 
 
 class Cache:
