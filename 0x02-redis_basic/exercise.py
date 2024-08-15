704d965f57ce24103b557f44eaabd7925479966d
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-08-15 04:26:31 +0100

    (fix): wrap method with a function wrapper
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -64,6 +64,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis database"""
